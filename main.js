d87ec2ca44cd1bd46484855a597a732cff83860a
akuabaw <akbarsaputra935@gmail.com>
2025-03-10 02:30:44 +0700

    lanjutkan brott

--- main.js
+++ main.js
@@ -3,14 +3,29 @@ console.log(angka);
 
 // document.querySelector(".number").textContent = angka;
 
+let score = 20;
+
 const pesan = function (message) {
   document.querySelector(".message").textContent = message;
 };
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  if (guess !== angka) {
+if (!guess){
+  pesan("Di isi dulu ya ganteng")
+}
+  //ini salah
+  else if(guess !== angka) {
+    if (score > 1){
     pesan(guess > angka ? "ketinggian" : "kerendahan");
+    score = score - 2;
+    document.querySelector('.score').textContent = score;
+    }else{
+    pesan("lu kalah banget");
+    document.querySelector("body").style.backgroundColor = "#ff8849";
+    document.querySelector('.score').textContent = 0;
+    }
+  //ini benar
   } else if (guess === angka) {
     pesan("lu benar");
     document.querySelector("body").style.backgroundColor = "#69be28";
@@ -18,12 +33,14 @@ document.querySelector(".check").addEventListener("click", function () {
 });
 
 document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
   angka = Math.trunc(Math.random() * 20) + 1;
 
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "#222";
   pesan("ayo tebak");
+  document.querySelector('.score').textContent = score;
 });
 
 // } else if (guess !== angka) {
@@ -34,3 +51,4 @@ document.querySelector(".again").addEventListener("click", function () {
 //   document.querySelector(".message").textContent = "Kerendahan kaya suku lu";
 // } else {
 //   document.querySelector(".message").textContent = "Ketinggian kaya gaya lu";
+
