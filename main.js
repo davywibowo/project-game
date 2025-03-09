c9b091b6674bd8be361b954ce5d194d88cc0c439
akuabaw <akbarsaputra935@gmail.com>
2025-03-10 01:37:12 +0700

    lanjutkan lah

--- main.js
+++ main.js
@@ -5,4 +5,14 @@ document.querySelector(".number").textContent = angka;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
+  if(!guess){
+    document.querySelector('.message').textContent = "Masukin duluu tolol bat dahh anjingg";
+  }else if (guess == angka){
+    document.querySelector('.message').textContent = "Bener, tapi bukan berarti lu jago di land of dawn";
+  }else if (guess < angka){
+    document.querySelector('.message').textContent = "Kerendahan kaya suku lu";
+  }else{
+    document.querySelector('.message').textContent = "Ketinggian kaya gaya lu";
+  }
+  
 });
