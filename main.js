2859f5acd2ab0c83123b69948ad2cc3ae19e4462
davywibowo <davywibowo10@gmail.com>
2025-03-10 02:43:01 +0700

    udah selesai nih

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let angka = Math.trunc(Math.random() * 20) + 1;
-console.log(angka);
-
-// document.querySelector(".number").textContent = angka;
-
 let score = 20;
+let scoreTertinggi = 0;
+
+console.log(angka);
 
 const pesan = function (message) {
   document.querySelector(".message").textContent = message;
@@ -11,44 +10,42 @@ const pesan = function (message) {
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-if (!guess){
-  pesan("Di isi dulu ya ganteng")
-}
+  if (!guess) {
+    pesan("Di isi dulu ya ganteng");
+    document.querySelector(".guess").disabled = true;
+    document.querySelector("body").style.backgroundColor = "#921";
+  }
   //ini salah
-  else if(guess !== angka) {
-    if (score > 1){
-    pesan(guess > angka ? "ketinggian" : "kerendahan");
-    score = score - 2;
-    document.querySelector('.score').textContent = score;
-    }else{
-    pesan("lu kalah banget");
-    document.querySelector("body").style.backgroundColor = "#ff8849";
-    document.querySelector('.score').textContent = 0;
+  else if (guess !== angka) {
+    if (score > 1) {
+      pesan(guess > angka ? "ketinggian" : "kerendahan");
+      score = score - 2;
+      document.querySelector(".score").textContent = score;
+    } else {
+      pesan("lu kalah banget put, jabar lebih ganteng");
+      document.querySelector("body").style.backgroundColor = "#921";
+      document.querySelector(".guess").disabled = true;
     }
-  //ini benar
+    //ini benar
   } else if (guess === angka) {
     pesan("lu benar");
     document.querySelector("body").style.backgroundColor = "#69be28";
+    if (score > scoreTertinggi) {
+      scoreTertinggi = score;
+      document.querySelector(".highscore").textContent = scoreTertinggi;
+    }
   }
 });
 
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   angka = Math.trunc(Math.random() * 20) + 1;
+  console.log(angka);
 
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "#222";
   pesan("ayo tebak");
-  document.querySelector('.score').textContent = score;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").disabled = false;
 });
-
-// } else if (guess !== angka) {
-//   document.querySelector(".message").textContent =
-//     "Bener, tapi bukan berarti lu jago di land of dawn";
-//   document.querySelector("body").style.backgroundColor = "#69be28";
-// } else if (guess < angka) {
-//   document.querySelector(".message").textContent = "Kerendahan kaya suku lu";
-// } else {
-//   document.querySelector(".message").textContent = "Ketinggian kaya gaya lu";
-
